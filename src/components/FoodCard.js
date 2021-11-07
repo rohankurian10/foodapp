87c17673f9e8610bf022805732ff435c59454a61
Rohan Kurian <rohankurian10@hotmail.com>
2021-11-07 21:24:00 +0530

    zzz

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -4,9 +4,12 @@ let FoodCard = () => {
   return (
     <>
       <div className="FoodCard">
-        <div className="Name"></div>
-        <div className="Description"></div>
-        <div className="Price"></div>
+        <div className="LeftSide">
+          <div className="Name"></div>
+          <div className="Description"></div>
+          <div className="Price"></div>
+        </div>
+        <div className="RightSide"></div>
       </div>
     </>
   );
